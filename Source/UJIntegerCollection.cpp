df45cfc4915cd32a791ce033575bcd5c3b03bd27
Luka Eardley <lukaeardley01@gmail.com>
2020-09-10 13:15:32 +0200

    First commit, code doesnt work. code is an outline and a work in progress

--- Source/UJIntegerCollection.cpp
+++ Source/UJIntegerCollection.cpp
@@ -1,1 +1,42 @@
 #include "UJIntegerCollection.h"
+///constructors
+UJIntegerCollection() {};
+UJIntegerCollection(int length)
+{
+    _length = length
+};
+UJIntegerCollection(const UJIntegerCollection & obj)
+{
+    _length = obj.getlength
+};
+UJIntegerCollection(int colcount)
+{
+    _colcount = colcount
+};
+///functions
+int UJIntegerCollection::getlength();
+{
+    return _length;
+}
+int UJIntegerCollection::colsum(colCount,colCopy);
+{
+    ///no clue
+    return colCount + colCopy;
+}
+
+
+
+int UJIntegerCollection::colSum()
+{
+    return 0;
+}
+
+int UJIntegerCollection::colBeforeInc()
+{
+    return 0;
+}
+
+int UJIntegerCollection::colSum()
+{
+    return 0;
+}
--- Source/UJIntegerCollection.h
+++ Source/UJIntegerCollection.h
@@ -1,5 +1,19 @@
 #ifndef UJINTEGERCOLLECTION_H
 using namespace std;
-
+class UJIntegerCollection;
+{
+public:
+    UJIntegerCollection()
+    UJIntegerCollection(int length)
+    UJIntegerCollection(const UJIntegerCollection & obj)
+    UJIntegerCollection(int colcount)
+    int getlength();
+private:
+    int _colcount[];
+    int _length;
+};
+///Prototype functions
+int getlength();
+int colSum(int colCount, int colCopy);
 
 #endif // UJINTEGERCOLLECTION_H

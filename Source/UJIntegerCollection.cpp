97aff0e9829504a1c644bd200b8227aac2cb61be
Luka Eardley <lukaeardley01@gmail.com>
2020-09-10 17:36:00 +0200

    Code done, it is now in the debugging stage

--- Source/UJIntegerCollection.cpp
+++ Source/UJIntegerCollection.cpp
@@ -1,42 +1,98 @@
+#include <iostream>
 #include "UJIntegerCollection.h"
 ///constructors
-UJIntegerCollection() {};
-UJIntegerCollection(int length)
+UJIntegerCollection::UJIntegerCollection() {}
+UJIntegerCollection::UJIntegerCollection(int length)
 {
-    _length = length
-};
-UJIntegerCollection(const UJIntegerCollection & obj)
+    _length = length;
+}
+UJIntegerCollection::UJIntegerCollection(const UJIntegerCollection& obj)
 {
-    _length = obj.getlength
-};
-UJIntegerCollection(int colcount)
-{
-    _colcount = colcount
-};
-///functions
-int UJIntegerCollection::getlength();
+    _length = obj.getLength
+}
+///function
+int UJIntegerCollection::getLength()
 {
     return _length;
 }
-int UJIntegerCollection::colsum(colCount,colCopy);
+///operators
+UJIntegerCollection& UJIntegerCollection::operator=(const UJIntegerCollection& obj)
 {
-    ///no clue
-    return colCount + colCopy;
+    for (int i = 0; i < obj.getlength(); i++)
+        _colcount[i] = obj[i];
+    _length = obj.getlength();
+    return *this;
+}
+int& UJIntegerCollection::operator[](const int index)
+{
+    if (index >= _length)
+    {
+        cout << "Out of bounds" << endl;
+        exit(0);
+    }
+    return _colcount[index];
+}
+int& UJIntegerCollection::operator()(const int val, const UJIntegerCollection& obj)
+{
+    int n =1;
+    sort(obj[], obj[] + n);
+
+    int max_count = 1;
+    int res = obj[0];
+    int curr_count = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (obj[i] == obj[i - 1])
+            curr_count++;
+        else
+        {
+            if (curr_count > max_count)
+            {
+                max_count = curr_count;
+                res = obj[i - 1];
+            }
+            curr_count = 1;
+        }
+    }
+    if (curr_count > max_count)
+    {
+        max_count = curr_count;
+        res = obj[n - 1];
+    }
+
+    return res;
+}
+UJIntegerCollection& UJIntegerCollection::operator+(const UJIntegerCollection& obj1, const UJIntegerCollection& obj2)
+{
+    if (obj1.getLength() != obj2.getLength())
+    {
+        cout << "Out of bounds" << endl;
+        exit(0);
+    }
+    UJIntegerCollection collection(obj1.getLength());
+    for (int i = 0; i < obj1.getLength(); i++)
+        collection[i] = obj1[i] + obj2[i];
+    return collection;
 }
 
-
-
-int UJIntegerCollection::colSum()
+UJIntegerCollection& UJIntegerCollection::operator+(const int val)
 {
-    return 0;
+    for (int i = 0; i < this->_length; i++)
+        this->_colcount[i] += val;
+    return *this;
 }
 
-int UJIntegerCollection::colBeforeInc()
+UJIntegerCollection& UJIntegerCollection::operator++(int)
 {
-    return 0;
+    for (int i = 0; i < this->_length; i++)
+        this->_colcount[i]++;
+    return *this;
 }
 
-int UJIntegerCollection::colSum()
+ostream& operator<<(ostream& output, const UJIntegerCollection& obj)
 {
-    return 0;
+    for (int i = 0; i < obj.getLength(); i++)
+        output << obj[i] << ' ';
+    output << endl;
+    return output;
 }
--- Source/UJIntegerCollection.h
+++ Source/UJIntegerCollection.h
@@ -1,19 +1,24 @@
 #ifndef UJINTEGERCOLLECTION_H
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-class UJIntegerCollection;
+class UJIntegerCollection
 {
 public:
-    UJIntegerCollection()
-    UJIntegerCollection(int length)
-    UJIntegerCollection(const UJIntegerCollection & obj)
-    UJIntegerCollection(int colcount)
-    int getlength();
+    UJIntegerCollection();
+    UJIntegerCollection(int length);
+    UJIntegerCollection(const UJIntegerCollection & obj);
+    int getLength();
+    UJIntegerCollection& operator=(const UJIntegerCollection& obj);
+    int& operator[](const int index);
+    int& operator()(const int val);
+    UJIntegerCollection& operator+(const UJIntegerCollection& obj1, const UJIntegerCollection& obj2);
+    UJIntegerCollection& operator+(const int val, const UJIntegerCollection& obj);
+    UJIntegerCollection& operator++(int);
+    friend ostream& operator<<(ostream& output, const UJIntegerCollection& obj);
+
 private:
     int _colcount[];
     int _length;
 };
-///Prototype functions
-int getlength();
-int colSum(int colCount, int colCopy);
-
 #endif // UJINTEGERCOLLECTION_H

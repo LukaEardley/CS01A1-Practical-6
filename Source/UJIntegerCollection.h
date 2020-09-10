e97e90258c7eeca96eaedf6ab5624a1d486828f8
Luka Eardley <lukaeardley01@gmail.com>
2020-09-10 12:05:41 +0200

    Initial commit

---
+++ Source/UJIntegerCollection.h
@@ -0,0 +1,5 @@
+#ifndef UJINTEGERCOLLECTION_H
+using namespace std;
+
+
+#endif // UJINTEGERCOLLECTION_H
---
+++ Source/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include "UJIntegerCollection.h"
+
+using namespace std;
+
+int main()
+{
+    UJIntegerCollection colCount(10);
+    for(unsigned int i = 0; i < colCount.getLength(); i++)
+        colCount[i] = i + 1;
+    UJIntegerCollection colCopy;
+    colCopy = colCount;
+    cout << colCopy << endl;  //Output should be 1 2 3 4 5 6 7 8 9 10
+    UJIntegerCollection colSum(colCount + colCopy);
+    cout << colSum << endl; //Output should be 2 4 6 8 10 12 14 16 18 20
+    colCopy[1] = 5;
+    colCopy[0] = 5;
+    cout << colCopy(5) << endl; //Output should be 3
+    UJIntegerCollection colBeforeInc;
+    colBeforeInc = colCount++;
+    cout << colBeforeInc << endl; //Output should be 1 2 3 4 5 6 7 8 9 10
+    cout << colCount << endl; //Output should be 2 3 4 5 6 7 8 9 10 11
+    cout << (2 + colCount) << endl; //Output should be 4 5 6 7 8 9 10 11 12 13
+    return 0;
+}
